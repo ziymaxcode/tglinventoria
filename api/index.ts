31f20a5f8e41ba2b619fe23ddc65b71118e64d62
ziymaxcode <mfosh29@gmail.com>
2026-05-27 16:44:43 +0530

    prod fix2

--- api/index.ts
+++ api/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { apiRouter } from '../server'; // Requires the compiled .js or runs natively via Vercel TS compilation depending on settings
 
 const app = express();
-app.use(apiRouter); // We stripped out the "/api" prefix from apiRouter, Vercel routes `/api/*` to here, so it works perfectly.
+app.use('/api', apiRouter);
+app.use('/', apiRouter);
 
 export default app;
