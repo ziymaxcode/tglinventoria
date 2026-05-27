769bf149c41869b99c777b03e4e98efa7a0ccc04
ziymaxcode <mfosh29@gmail.com>
2026-05-27 17:01:55 +0530

    prod fix3

--- server.ts
+++ server.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import path from "path";
-import { createServer as createViteServer } from "vite";
 import { google } from "googleapis";
 
 export const apiRouter = express.Router();
@@ -422,6 +421,7 @@ async function startServer() {
 
   // --- Vite Middleware for Development ---
   if (process.env.NODE_ENV !== "production") {
+    const { createServer: createViteServer } = await import("vite");
     const vite = await createViteServer({
       server: { middlewareMode: true },
       appType: "spa",
